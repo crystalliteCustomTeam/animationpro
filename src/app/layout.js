6ae9be27201575f3325413854d458f8cef72a7d7
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-02-13 19:59:09 +0500

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="robots" content="noindex"></meta>
+      <meta name="robots" content="noindex "></meta>
       <body className={`${primary.variable}`}>
         <main>{children}</main>
       </body>
